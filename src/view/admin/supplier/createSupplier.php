1926cece305384ddb8f23933cafb4d6a4bfc80f0
Nguyễn Quang Huy <nqhuywork.dev@gmail.com>
2026-09-15 17:01:02 +0700

    Update createSupplier.php

--- src/view/admin/supplier/createSupplier.php
+++ src/view/admin/supplier/createSupplier.php
@@ -76,14 +76,16 @@ function loadPageDependOnFeature() {
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="/UTH-PHP/src/view/admin/supplier/createSupplier.css" />
   <title>Document</title>
-</head>
+<link rel="stylesheet" href="/UTH-PHP/src/view/assets/css/admin-responsive.css?v=20260914">
+  <script src="/UTH-PHP/src/view/assets/js/admin-responsive.js?v=20260914" defer></script>
+  </head>
 
 <body>
   <div id="root">
