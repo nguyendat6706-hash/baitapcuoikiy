b12c85c8269a10c1cc68d2f636490656231d3085
Nguyễn Quang Huy <nqhuywork.dev@gmail.com>
2026-09-15 14:47:59 +0700

    Update EditProductController.php

--- src/controller/ProductController/EditProductController.php
+++ src/controller/ProductController/EditProductController.php
@@ -3,16 +3,13 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $projectRoot = $_SERVER['DOCUMENT_ROOT'] . '/UTH-PHP';
 require_once "$projectRoot/src/model/ProductModels/SanPhamModel.php";
-
 class EditProductController {
   public $maSanPham;
-
   public function __construct() {
     if (isset($_GET['maSanPham'])) {
       $this->maSanPham = ($_GET['maSanPham']);
     }
   }
-
   public function display() {
     global $projectRoot;
     require_once "$projectRoot/src/model/ProductModels/SanPhamModel.php";
@@ -20,11 +17,9 @@ class EditProductController {
     $modelLoaiSanPham = new LoaiSanPham();
     $dataGetLoaiSanPham = $modelLoaiSanPham->getAllTypeProduct(null, null)->data;
     $modelProduct = new ProductModel();
-
     $dataProduct = $modelProduct->getProductById($this->maSanPham)->data; // Sử dụng $this->maSanPham
     require "$projectRoot/src/view/admin/product/editProduct.php";
   }
-
   public function updateRequest() {
     $modelProduct = new ProductModel();
     // Kiểm tra xem yêu cầu có phải là Ajax không
@@ -56,8 +51,11 @@ class EditProductController {
             $anhMinhHoa = $_POST['AnhMinhHoa'];
             $maLoaiSanPham = $_POST['loaiSanPham'];
 
+            // ==== Mô tả sản phẩm lấy từ CKEditor (Quang Huy) ====
+            $moTa = isset($_POST['MoTa']) ? $_POST['MoTa'] : '';
+
             // Thực hiện cập nhật sản phẩm
-            $result = $modelProduct->updateProduct($maSanPham, $tenSP, $theTich, $gia, $nongDoCon, $xuatXu, $thuongHieu, $anhMinhHoa, $maLoaiSanPham, 1);
+            $result = $modelProduct->updateProduct($maSanPham, $tenSP, $theTich, $gia, $nongDoCon, $xuatXu, $thuongHieu, $anhMinhHoa, $maLoaiSanPham, $moTa);
 
             // Kiểm tra kết quả và trả về phản hồi dưới dạng JSON
             if ($result->status == 200) {
@@ -76,7 +74,6 @@ class EditProductController {
       }
     }
   }
-
   public function show() {
     if (isset($_POST['action']) && $_POST['action'] === 'updateProduct') {
       $this->updateRequest();
@@ -85,6 +82,5 @@ class EditProductController {
     }
   }
 }
-
 $editProductController = new EditProductController();
 $editProductController->show();
