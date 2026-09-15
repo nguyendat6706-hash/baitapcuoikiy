2481547046776818f7b1c1cecc4d316be9ce887f
Nguyễn Quang Huy <nqhuywork.dev@gmail.com>
2026-09-15 16:59:56 +0700

    Update UpdateUserView.php

--- src/view/admin/user/UpdateUserView.php
+++ src/view/admin/user/UpdateUserView.php
@@ -65,7 +65,7 @@ function loadPageDependOnFeature() {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 
 <head>
   <meta charset="UTF-8" />
@@ -76,7 +76,9 @@ function loadPageDependOnFeature() {
   <!-- <link rel="stylesheet" href="../view/assets/Admin.css" /> -->
 
   <title>Document</title>
-</head>
+<link rel="stylesheet" href="/UTH-PHP/src/view/assets/css/admin-responsive.css?v=20260914">
+  <script src="/UTH-PHP/src/view/assets/js/admin-responsive.js?v=20260914" defer></script>
+  </head>
 
 <body>
   <div id="root">
