fb973300eafdbe54064934e2731bafc693202f10
Nguyễn Quang Huy <nqhuywork.dev@gmail.com>
2026-09-15 17:05:27 +0700

    Update ThongKeTongQuat.php

--- src/view/admin/ThongKe/ThongKeTongQuat.php
+++ src/view/admin/ThongKe/ThongKeTongQuat.php
@@ -73,7 +73,7 @@ function loadPageDependOnFeature() {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 
 <head>
   <meta charset="UTF-8" />
@@ -87,7 +87,9 @@ function loadPageDependOnFeature() {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
 
   <title>Thống kê tổng quát</title>
-</head>
+<link rel="stylesheet" href="/UTH-PHP/src/view/assets/css/admin-responsive.css?v=20260914">
+  <script src="/UTH-PHP/src/view/assets/js/admin-responsive.js?v=20260914" defer></script>
+  </head>
 
 <body>
   <div id="root">
