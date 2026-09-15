1fff79041dda4cfd27f9f05309c013bea539b322
Nguyễn Quang Huy <nqhuywork.dev@gmail.com>
2026-09-15 17:02:39 +0700

    Update supplierView.php

--- src/view/admin/supplier/supplierView.php
+++ src/view/admin/supplier/supplierView.php
@@ -81,7 +81,7 @@ function loadPageDependOnFeature() {
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 
 <head>
   <meta charset="UTF-8" />
@@ -90,7 +90,9 @@ function loadPageDependOnFeature() {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="/UTH-PHP/src/view/admin/supplier/supplierView.css">
   <title>Document</title>
-</head>
+<link rel="stylesheet" href="/UTH-PHP/src/view/assets/css/admin-responsive.css?v=20260914">
+  <script src="/UTH-PHP/src/view/assets/js/admin-responsive.js?v=20260914" defer></script>
+  </head>
 
 <body>
   <div id="root">
