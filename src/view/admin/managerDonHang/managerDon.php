aefda4361ad1b6c8fde0207c7b0952b0ad17fd2e
Nguyễn Quang Huy <nqhuywork.dev@gmail.com>
2026-09-15 17:09:53 +0700

    Change HTML language attribute to Vietnamese

--- src/view/admin/managerDonHang/managerDon.php
+++ src/view/admin/managerDonHang/managerDon.php
@@ -77,7 +77,7 @@ function loadPageDependOnFeature() {
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 
 <head>
   <meta charset="UTF-8" />
@@ -87,7 +87,9 @@ function loadPageDependOnFeature() {
   <link rel="stylesheet" href="/UTH-PHP/view/admin/InventoryView/adminDemo.css" />
   <link rel="stylesheet" href="/UTH-PHP/src/view/admin/managerDonHang/qldonhang.css" />
   <title>Document</title>
-</head>
+<link rel="stylesheet" href="/UTH-PHP/src/view/assets/css/admin-responsive.css?v=20260914">
+  <script src="/UTH-PHP/src/view/assets/js/admin-responsive.js?v=20260914" defer></script>
+  </head>
 
 <body>
   <div>
